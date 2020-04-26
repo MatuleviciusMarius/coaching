d3ff9683b5b7c09c580e089db31d74b2675b27ff
Marius <marius.matulevicius1@gmail.com>
2020-04-26 14:22:44 +0100

    Updates

---
+++ precache-manifest.d9e3be3b2743e60411ebf1a160fdb2cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d430bfa03dbdf8de6134cc74bcf0dc7",
+    "url": "/MatuleviciusMarius/coaching.git/index.html"
+  },
+  {
+    "revision": "a5ebba3f69bff33cc38e",
+    "url": "/MatuleviciusMarius/coaching.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eb769038f890f8ee7556",
+    "url": "/MatuleviciusMarius/coaching.git/static/js/2.a2d077c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MatuleviciusMarius/coaching.git/static/js/2.a2d077c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ebba3f69bff33cc38e",
+    "url": "/MatuleviciusMarius/coaching.git/static/js/main.b69b2820.chunk.js"
+  },
+  {
+    "revision": "3917a3b2fc19df008ba3",
+    "url": "/MatuleviciusMarius/coaching.git/static/js/runtime-main.ea2da0c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MatuleviciusMarius/coaching.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
