2132c495ca1ed19a77393ae3d52a8fd2f51d29ab
Marius <marius.matulevicius1@gmail.com>
2020-05-12 15:40:08 +0100

    carousel update

--- index.js
+++ index.js
@@ -70,9 +70,11 @@ function createSlide() {
       },
       lazyLoad: true,
       drag: true,
-      focus: "center",
       speed: 800,
       lazyLoad: "nearby",
+      gap: 0,
+      focus: "center",
+      height: "50%",
     }).mount();
   } else {
     new Splide(".splide", {
@@ -80,6 +82,7 @@ function createSlide() {
       rewind: true,
       lazyLoad: true,
       drag: true,
+      autoplay: true,
     }).mount();
   }
 }
@@ -87,3 +90,35 @@ function createSlide() {
 function closeButton() {
   bottomTextBox.remove("displayInlineBlock");
 }
+
+var autoSizeText;
+
+autoSizeText = function () {
+  var el, elements, _i, _len, _results;
+  elements = $(".resize");
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push(
+      (function (el) {
+        var resizeText, _results1;
+        resizeText = function () {
+          var elNewFontSize;
+          elNewFontSize =
+            parseInt($(el).css("font-size").slice(0, -2)) - 1 + "px";
+          return $(el).css("font-size", elNewFontSize);
+        };
+        _results1 = [];
+        while (el.scrollHeight > el.offsetHeight) {
+          _results1.push(resizeText());
+        }
+        return _results1;
+      })(el)
+    );
+  }
+  return _results;
+};
